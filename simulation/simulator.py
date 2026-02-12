8045836ee04a7a80beca722aad9036353ecfa7a3
Victor Angielczyk <44138823+VictorAngielczyk@users.noreply.github.com>
2026-02-12 15:15:38 -0500

    all changes for assignment
    created __init__.py for viz and simulated. Removed all hardcoded function params and added defaults, switched to numpy ndarrays, added typing for all files.

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,25 +1,27 @@
+import numpy as np
 
-def simulate_seir(parameters, init_conditions, days=51):
-    """TODO"""
 
-    # Extract parameters and initial conditions (Your code here)
+def simulate_seir(
+    parameters: tuple[float, float, float] = (3, 0.5, 0.5),
+    init_conditions: tuple[float, float, float, float] = (9999., 1., 0., 0.),
+    days: int = 51,
+) -> tuple[np.ndarray[np.float64], np.ndarray[np.float64], np.ndarray[np.float64], np.ndarray[np.float64]]:
+
     beta, sigma, gamma = parameters
     S0, E0, I0, R0 = init_conditions
     N = S0 + E0 + I0 + R0
-    S = [S0] + [None] * (days-1)
-    E = [E0] + [None] * (days-1)
-    I = [I0] + [None] * (days-1)
-    R = [R0] + [None] * (days-1)
-    
-    # For each day, perform SEIR update
-    for t in range(1, days):
 
-        # Compute new cases and update equations
+    S = np.empty(days, dtype=np.float64)
+    E = np.empty(days, dtype=np.float64)
+    I = np.empty(days, dtype=np.float64)
+    R = np.empty(days, dtype=np.float64)
+    S[0], E[0], I[0], R[0] = S0, E0, I0, R0
+
+    for t in range(1, days):
         E_new = (beta*S[t-1]*I[t-1]) / N
         I_new = sigma*E[t-1]
         R_new = gamma*I[t-1]
-      
-        # Update equations
+
         S[t] = S[t-1] - E_new
         E[t] = E[t-1] + E_new - I_new
         I[t] = I[t-1] + I_new - R_new
